b8629e1239a4bce51ae6f1e40cb6637a70349e4b
vincordian <vincar100@icloud.com>
2025-11-25 16:14:40 -0600

    Update DataHandler.lua

--- ServerStorage/Modules/DataHandler.lua
+++ ServerStorage/Modules/DataHandler.lua
@@ -1,14 +1,35 @@
-local player = game.Players.LocalPlayer
-local plrGui = player.PlayerGui
+local LoadGuiRemote = game:GetService("ReplicatedStorage"):WaitForChild("Remotes").ServerToClient.LoadGuiRemote
 
-local gui = plrGui:WaitForChild("Inventory")
-local uis = game:GetService("UserInputService")
+local handler = {}
+
+handler.Profiles = {}
 
 
-uis.InputBegan:Connect(function(keydown, gp)
-	if gp then return end
-	
-	if keydown.KeyCode == Enum.KeyCode.Q then
-		gui.Enabled = not gui.Enabled
+local function getDupes(plr, attributes:string)
+
+	local profile = handler.Profiles[plr]
+
+	if profile and profile.Data and profile.Data.Inventory then
+		
+		if profile.Data.Inventory[attributes] then
+			profile.Data.Inventory[attributes] += 1
+		else
+			profile.Data.Inventory[attributes] = 1
+		end
+		
+		LoadGuiRemote:FireClient(plr, attributes, 1)
+	else
+		warn("Player", plr, "has received an interal error loading data. Please report this.")
 	end
-end)
+end
+
+
+function handler.giveNone(plr)
+	getDupes(plr, "None")
+end
+
+function handler.giveShape(plr, attributes:string)
+	getDupes(plr, attributes)
+end
+
+return handler
